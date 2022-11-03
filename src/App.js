5b9c48f98e0445405668824470f6fd92d616fcec
sabrinabu <sabrinakhaled13@gmail.com>
2022-11-03 19:57:51 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [formFields, setFormFiels] = useState([
+    {
+      name: "",
+      age: "",
+    },
+  ]);
+
+  const [butt, setButt] = useState(false);
+
+  
+
+  const handleMore = () => {
+    const obj = {
+      name: "",
+      age: "",
+    };
+    setFormFiels([...formFields, obj]);
+    setButt(false);
+  };
+
+  const handleRemove = () => {
+    const data = [...formFields];
+    console.log(data);
+    if (formFields.length <= 2) {
+      setButt(true);
+    } 
+    data.splice(formFields.length - 1);
+
+    setFormFiels(data);
+  };
+
   return (
     <div className="App">
-      <h1>first commit</h1>
+      {formFields.map((form) => (
+        <div>
+          <input name="name" placeholder="name" />
+          <input name="age" placeholder="age" />
+        </div>
+      ))}
+
+      <button onClick={handleMore} className="add">Add more</button>
+      <button onClick={handleRemove} disabled={butt}>
+        delete
+      </button>
     </div>
   );
 }
